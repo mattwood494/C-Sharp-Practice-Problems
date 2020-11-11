41dde8f21f9095d085511c4330cc14523c2e1ce8
Matthew Wood <matthew.wood_28@outlook.com>
2020-11-11 10:43:37 -0800

    Creating a Division method
    Create a Method that accepts two integers and returns the integer result of “firstArg / secondArg”.
    
    Also swap the values of the arguments so that the method calling this one will be affected.
    
    Any exception needs to be caught by the method which will then return ‘0’.

---
+++ Swaps/Swaps/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Swaps
+{
+    class Program
+    {
+
+        /* Problem 2:
+
+        Create a Method that accepts two integers and returns the integer result of “firstArg / secondArg”.
+
+        Also swap the values of the arguments so that the method calling this one will be affected.
+
+        Any exception needs to be caught by the method which will then return ‘0’.
+
+        */
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Division(10, 5));
+            Console.WriteLine(Division(5, 10)); // returns 0 because 5/10 is .5
+            Console.WriteLine(Division(10, 0)); 
+            Console.WriteLine(Division(657489, 5)); 
+
+           
+        }
+        static int Division(int firstArg, int secondArg)
+        {
+            int answer;
+
+            try 
+            {
+                answer = firstArg / secondArg;
+            }
+            catch (Exception) // if an excepetion is caught, then program will display a 0.
+            {
+
+                return 0;
+            }
+
+            return answer;
+        }
+        
+    }
+}
