3e6eadc2926fcc5cc74eabf8497867497fdd2253
Matthew Wood <matthew.wood_28@outlook.com>
2020-11-11 09:51:50 -0800

    More data structures and algorithms practice

---
+++ Defining Grade/Defining Grade/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Defining_Grade
+{
+    class Program
+    {
+
+        /*Write a program that will accept a grade of 0 - 100 and displays the correct Letter 
+         * grade based on This course’s syllabus. (97-100 => A+, …)
+         * 
+         * 
+        */
+
+
+        static void Main(string[] args)
+        {
+            //Call the method
+            DisplayLetterGrade(96);
+        }
+
+        static void DisplayLetterGrade(int percentage)
+        {
+            //Test to see what range the input falls into and display the correlated letter grade    
+            if (percentage > 100)
+            {
+                Console.WriteLine("What?! Over 100?!?!?!");
+            }
+            else if (percentage >= 97 && percentage <= 100)
+            {
+                Console.WriteLine("A+");
+            }
+            else if (percentage >= 93 && percentage <= 96)
+            {
+                Console.WriteLine("A");
+            }
+            else if (percentage >= 90 && percentage <= 92)
+            {
+                Console.WriteLine("A-");
+            }
+            else if (percentage >= 87 && percentage <= 89)
+            {
+                Console.WriteLine("B+");
+            }
+            else if (percentage >= 83 && percentage <= 86)
+            {
+                Console.WriteLine("B");
+            }
+            else if (percentage >= 80 && percentage <= 82)
+            {
+                Console.WriteLine("B-");
+            }
+            else if (percentage >= 77 && percentage <= 79)
+            {
+                Console.WriteLine("C+");
+            }
+            else if (percentage >= 73 && percentage <= 76)
+            {
+                Console.WriteLine("C");
+            }
+            else if (percentage >= 70 && percentage <= 72)
+            {
+                Console.WriteLine("C-");
+            }
+            else if (percentage >= 67 && percentage <= 69)
+            {
+                Console.WriteLine("D+");
+            }
+            else if (percentage >= 63 && percentage <= 66)
+            {
+                Console.WriteLine("D");
+            }
+            else if (percentage >= 60 && percentage <= 62)
+            {
+                Console.WriteLine("D-");
+            }
+            else if (percentage >= 0 && percentage <= 59)
+            {
+                Console.WriteLine("F");
+            }
+            else
+            {
+                Console.WriteLine("Whoa...");
+            }
+            
+            
+        }
+    }
+}
---
+++ Reverse Integer/Reverse Integer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Reverse_Integer
+{
+    /*
+     * Given a 32-bit signed integer, reverse digits of an integer.
+
+        Assumptions:
+            We are dealing with an environment that could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1]. 
+            Your function returns 0 when the reversed integer overflows.
+
+ 
+
+        Example 1:
+
+            Input: x = 123
+            Output: 321
+
+        Example 2:
+
+            Input: x = -123
+            Output: -321
+
+        Example 3:
+
+            Input: x = 120
+            Output: 21
+
+        Example 4:
+
+            Input: x = 0
+            Output: 0
+ 
+
+        Constraints:
+
+        -2^31 <= x <= 2^31 - 1
+
+    `   
+            
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Pseudocode:
+             * 
+             * Create a method Reverse that takes an integer as input and returns an integer
+             * 
+             * Create an array that can hold the exact amount of integers that are inside the input
+             */
+
+
+        }
+    }
+}
