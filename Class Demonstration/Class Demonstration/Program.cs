031cbce42cb87df19dae2bc027c7260d74b7ed9b
Matthew Wood <matthew.wood_28@outlook.com>
2020-11-11 19:54:15 -0800

    Class Demonstration
    These files demonstrate how one can get and set properties while also limiting what a given property can be set as.

---
+++ Class Demonstration/Class Demonstration/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Class_Demonstration
+{
+    /*Create a class with a handful of properties and a 
+     * single constructor that has default values for all 
+     * but one of the properties. Make sure that each 
+     * property only accepts a restricted range of 
+     * values. In your comments tell what ranges you 
+     * properties accept
+     * 
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Teenager
+    {
+
+        
+
+        private int _age; //restricted age is 13 - 19
+
+        public int Age
+        {
+            get { return _age; }
+            set 
+            {
+                if (value < 20 && value > 12)
+                {
+                    _age = value;
+                }
+                
+            }
+        }
+
+        public string _school { get; set; } //school value is unrestricted
+        private int _grade; // grade level is restricted to 6 - 12
+
+        public int Grade
+        {
+            get { return _grade; }
+            set 
+            {
+                if (value > 6 && value < 12)
+                {
+                    _grade = value;
+                }
+                
+            }
+        }
+
+        private double _gpa; // gpa is restricted to be between 0 and 4
+
+        public double GPA
+        {
+            get { return _gpa; }
+            set 
+            {
+                if (value >= 0 && value <= 4)
+                {
+                    _gpa = value;
+                }
+                 
+            }
+        }
+
+        public Teenager(int age, int grade, double gpa)
+        {
+            _age = age;
+            _grade = grade;
+            _gpa = gpa;
+        }
+
+
+
+
+
+
+
+
+    }
+
+    
+}
---
+++ TestClass/TestClass/5Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace TestClass
+{
+    /*
+     * Problem 5:
+
+        Create a Test project that tests the ranges of the 
+        previous class to ensure it only accepts values in the 
+        given ranges
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teenager joe = new Teenager(99, 543, 200);
+            Console.WriteLine(joe.Age);
+            Console.WriteLine(joe.Grade);
+            Console.WriteLine(joe.GPA);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Teenager bob = new Teenager(18, 9, 3.29);
+            Console.WriteLine(bob.Age);
+            Console.WriteLine(bob.Grade);
+            Console.WriteLine(bob.GPA);
+
+
+        }
+    }
+
+    public class Teenager
+    {
+
+
+
+        private int _age = -1; //restricted age is 13 - 19
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value < 20 && value > 12)
+                {
+                    _age = value;
+                }
+
+            }
+        }
+
+        public string _school { get; set; } //school value is unrestricted
+        private int _grade = -1; // grade level is restricted to 6 - 12
+
+        public int Grade
+        {
+            get { return _grade; }
+            set
+            {
+                if (value > 6 && value < 12)
+                {
+                    _grade = value;
+                }
+
+            }
+        }
+
+        private double _gpa = -1.0; // gpa is restricted to be between 0 and 4
+
+        public double GPA
+        {
+            get { return _gpa; }
+            set
+            {
+                if (value >= 0 && value <= 4)
+                {
+                    _gpa = value;
+                }
+
+            }
+        }
+
+        public Teenager(int age, int grade, double gpa)
+        {
+            Age = age;
+            Grade = grade;
+            GPA = gpa;
+        }
+
+
+
+
+
+
+
+
+    }
+}
